6769a14197dfdb33aa56059bf634eadff99d2f59
ragielly <ragielly.19@gmail.com>
2025-10-28 09:29:18 -0300

    criando classe conta

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,5 +1,23 @@
-import 'package:anybank/anybank.dart' as anybank;
 
-void main(List<String> arguments) {
-  print('Hello world: ${anybank.calculate()}!');
+void main() {
+ Conta contaMatheus = Conta("Matheus", 1000);
+ Conta contaRoberta = Conta("Roberta", 2000);
+
+ List<Conta> contas = <Conta>[contaMatheus,contaRoberta];
+
+ for (Conta conta in contas) {
+    print(conta.titular);
+    print(conta.saldo);
+   
+ }
+
+ contaRoberta.saldo=5000;
+
+ print(contaRoberta.saldo);
+}
+class Conta{
+    String titular;
+    double saldo;
+
+    Conta(this.titular, this.saldo);
 }
