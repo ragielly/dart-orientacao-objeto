b36835f037a938c820a194185e4f83482ffd7b34
ragielly <ragielly.19@gmail.com>
2025-10-29 17:38:30 -0300

    aplicando herança e polimorfismo

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -4,6 +4,8 @@ import 'package:anybank/conta.dart';
 void main() {
  Conta contaMatheus = Conta("Matheus", 1000);
  Conta contaRoberta = Conta("Roberta", 2000);
+ ContaCorrente conta1 = ContaCorrente("Chris", 4000);
+ ContaPoupanca conta2 = ContaPoupanca("Denize", 4000);
 
  //List<Conta> contas = <Conta>[contaMatheus,contaRoberta];
 
@@ -11,5 +13,11 @@ void main() {
  contaRoberta.receber(3000);
  contaMatheus.receber(500);
  contaMatheus.enviar(200);
+ conta1.imprimirSaldo();
+ conta1.enviar(4300);
+ conta2.imprimirSaldo();
+ conta2.enviar(4300);
+ conta2.calcularRendimento();
+ conta2.imprimirSaldo();
 
 }
--- lib/conta.dart
+++ lib/conta.dart
@@ -20,3 +20,25 @@ class Conta{
       print("O Titular: $titular Saldo: R\$ $_saldo");
     }
 }
+class ContaCorrente extends Conta{
+  double emprestimo = 300;
+  ContaCorrente(super.titular,super._saldo);
+
+  @override
+   void enviar(double valor){
+        if(_saldo + emprestimo >= valor){
+         _saldo -= valor;
+         imprimirSaldo();      
+        }
+    }
+}
+class ContaPoupanca extends Conta{
+  double rendimento = 0.05;
+
+  ContaPoupanca(super.titular,super._saldo);
+
+  void calcularRendimento(){
+    _saldo += _saldo * rendimento;
+  }
+
+}
