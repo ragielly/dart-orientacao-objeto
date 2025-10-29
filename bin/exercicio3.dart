c2e647306b2952763c2870f1add8f8f34ebb2e3f
ragielly <ragielly.19@gmail.com>
2025-10-29 17:47:12 -0300

    enviando exercicios 3 e 4

---
+++ bin/exercicio3.dart
@@ -0,0 +1,10 @@
+// //1. Criando classes de gatos e cachorros
+// Crie uma classe Animal com um método emitirSom(). Em seguida,
+//  crie duas outras classes: Cachorro e Gato, que herdam o método 
+//  da classe Animal. Ao adicionar o método emitirSom() nas classes
+//  Cachorro e Gato, utilize a anotação @override para indicar que estão
+//   sobrescrevendo o método — o método emitirSom deve fazer um print
+//    “O cachorro late” para Cachorro e “o gato miou” para Gato.
+
+// Além disso, adicione métodos específicos para cada classe,
+//  como abanarRabo() para o Cachorro e arranharMoveis() para o Gato.
---
+++ bin/exercicio4.dart
@@ -0,0 +1,15 @@
+// 2. Produzindo uma conta salário com herança
+// Continuando o projeto Any Bank, é hora de criar um terceiro tipo de conta: 
+// a conta salário. Assim como as classes conta corrente e conta poupança,
+//  ela possui duas características essenciais: o titular e o saldo.
+
+// Escreva o código da classe ContaSalario e um objeto de conta salário
+// de uma pessoa chamada Catarina (ou qualquer nome que você desejar). Se quiser,
+//  imprima o saldo da pessoa no terminal.
+
+// Dicas:
+
+// Crie o código da classe ContaSalario e faça a instanciação 
+// do objeto contaSalarioCatarina;
+// Empregue a técnica de herança para evitar a repetição de código;
+// Apenas declare a classe. Não é preciso fazer nada além disso.
