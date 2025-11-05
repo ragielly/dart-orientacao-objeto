c692a271e623e26c2d1365439e217072b38977f2
ragielly <ragielly.19@gmail.com>
2025-11-05 16:52:59 -0300

    enviando exercicios

---
+++ bin/exercicio6.dart
@@ -0,0 +1,11 @@
+//5. Criando um programa de registro de funcionários
+//Primeiro, crie uma classe abstrata funcionario com os atributos nome de funcionário(a) e um double “salário base” inicializado em 3000 reais, e também um método calcularSalario. Em seguida, implemente, a partir de funcionario, as classes analista, gerente e diretor; sobrescreva o método calcularSalario nelas com os seguintes valores:
+
+// analista: salário base = salário base + salário base multiplicado por 1.2;
+// gerente: salário base = salário base + salário base multiplicado por 1.5;
+// diretor: salário base = salário base + salário base multiplicado por 2.0.
+// Por fim, imprima o cálculo de salário dos funcionários João (analista), Maria (gerente) e Giovana (diretora), conforme o exemplo a seguir:
+
+// Salário do Analista João: R$ 6600.0
+// Salário da Gerente Maria: R$ 7500.0   
+// Salário da Diretora Giovana: R$ 9000.0
---
+++ bin/exercicio7.dart
@@ -0,0 +1,13 @@
+// 6. Criando um programa informativo de espécies de seres vivos
+// Este exercício será um pouco mais desafiador.
+
+// Crie um programa que mostra características sobre os seres vivos e implemente três classes, seguindo os requisitos de cada uma:
+// Classe SerVivo: será uma classe abstrata que terá um método void mostrarCaracteristicas();.
+// Planta: será uma classe que implementa SerVivo e terá os seguintes objetos e suas descrições de print:
+// Girassol: é uma planta, não tem os cinco sentidos, é uma flor;
+// Laranjeira: é uma planta, não tem os cinco sentidos, é uma árvore;
+// Animal: é vivo e tem os cinco sentidos:
+// Ser humano: tem cinco sentidos, pensa e fala, é bípede e vive em média 75 anos;
+// Gato: tem cinco sentidos, mia, é quadrúpede e vive em média 20 anos;
+// Cão: tem cinco sentidos, late, é quadrúpede e vive em média 20 anos.
+// Represente essas coisas do mundo real com classe, classe abstrata, herança e polimorfismo de orientação a objetos.
