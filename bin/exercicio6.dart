5a220f05720e66d4a6b908b3fb1c8c28f12ca566
ragielly <ragielly.19@gmail.com>
2025-11-11 07:54:59 -0300

    adicionando exercicios resolvidos

--- bin/exercicio6.dart
+++ bin/exercicio6.dart
@@ -1,5 +1,6 @@
 //5. Criando um programa de registro de funcionários
-//Primeiro, crie uma classe abstrata funcionario com os atributos nome de funcionário(a) e um double “salário base” inicializado em 3000 reais, e também um método calcularSalario. Em seguida, implemente, a partir de funcionario, as classes analista, gerente e diretor; sobrescreva o método calcularSalario nelas com os seguintes valores:
+//Primeiro, crie uma classe abstrata funcionario com os atributos nome de funcionário(a) e um double “salário base” inicializado em 3000 reais, e também um método calcularSalario. 
+//Em seguida, implemente, a partir de funcionario, as classes analista, gerente e diretor; sobrescreva o método calcularSalario nelas com os seguintes valores:
 
 // analista: salário base = salário base + salário base multiplicado por 1.2;
 // gerente: salário base = salário base + salário base multiplicado por 1.5;
@@ -9,3 +10,57 @@
 // Salário do Analista João: R$ 6600.0
 // Salário da Gerente Maria: R$ 7500.0   
 // Salário da Diretora Giovana: R$ 9000.0
+
+
+void main(){
+
+   Analista a = Analista("Joao");
+   Gerente g = Gerente("Maria");
+   Diretor d = Diretor("Giovana");
+   a.calcularSalario();
+   g.calcularSalario();
+   d.calcularSalario();
+  // print(a.salario);
+  
+
+}
+
+abstract class Funcionario{
+  String nome;
+  double salario;
+  
+
+  Funcionario(this.nome) : salario=3000; // iniciando uma variavel com valor
+
+  void calcularSalario(){
+
+  }
+}
+
+class Analista extends Funcionario{
+  Analista(super.nome);
+// analista: salário base = salário base + salário base multiplicado por 1.2;
+@override
+  void calcularSalario() {
+    salario = salario + (salario * 1.2);
+    print("Salário do Analista $nome: R\$$salario");
+  }
+}
+class Gerente extends Funcionario{
+  Gerente(super.nome);
+// gerente: salário base = salário base + salário base multiplicado por 1.5;
+@override
+  void calcularSalario() {
+    salario = salario + (salario * 1.5);
+    print("Salário do Analista $nome: R\$$salario");
+  }
+}
+class Diretor extends Funcionario{
+  Diretor(super.nome);
+// diretor: salário base = salário base + salário base multiplicado por 2.0.
+@override
+  void calcularSalario() {
+   salario = salario + (salario * 2.0);
+   print("Salário do Analista $nome: R\$$salario");
+  }
+}
--- bin/exercicio7.dart
+++ bin/exercicio7.dart
@@ -4,10 +4,49 @@
 // Crie um programa que mostra características sobre os seres vivos e implemente três classes, seguindo os requisitos de cada uma:
 // Classe SerVivo: será uma classe abstrata que terá um método void mostrarCaracteristicas();.
 // Planta: será uma classe que implementa SerVivo e terá os seguintes objetos e suas descrições de print:
-// Girassol: é uma planta, não tem os cinco sentidos, é uma flor;
+// Girassol: ;
 // Laranjeira: é uma planta, não tem os cinco sentidos, é uma árvore;
 // Animal: é vivo e tem os cinco sentidos:
 // Ser humano: tem cinco sentidos, pensa e fala, é bípede e vive em média 75 anos;
 // Gato: tem cinco sentidos, mia, é quadrúpede e vive em média 20 anos;
 // Cão: tem cinco sentidos, late, é quadrúpede e vive em média 20 anos.
 // Represente essas coisas do mundo real com classe, classe abstrata, herança e polimorfismo de orientação a objetos.
+
+void main(){
+  Planta planta1 = Planta("Girassol", "Flor");
+  Planta planta2 = Planta("Pinheiro", "Arvore");
+  Animal animal1 = Animal("Humano", "Fala", 75);
+
+  planta1.mostrarCaracteristicas();
+  planta2.mostrarCaracteristicas();
+
+
+}
+
+abstract class SerVivo{
+  String nome;
+  String tipo;
+
+  SerVivo(this.nome, this.tipo);
+  void mostrarCaracteristicas(){ 
+  }
+}
+
+class Planta extends SerVivo{
+  
+  Planta(String nome, String tipo) : super( nome ,tipo );
+  @override
+  void mostrarCaracteristicas() {
+     print("$nome : é uma planta, não tem os cinco sentidos, é uma $tipo");
+  }
+
+}
+class Animal extends SerVivo{
+  int estimativa;
+  Animal(String nome, String tipo, this.estimativa) : super( nome ,tipo );
+  @override
+  void mostrarCaracteristicas() {
+    return print("$nome : tem cinco sentidos, $tipo, é quadrúpede e vive em média $estimativa anos");
+  }
+
+}
