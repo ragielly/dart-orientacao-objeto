c4829c6be14c1b7e5f5eca00284b7a5808f7148d
ragielly <ragielly.19@gmail.com>
2025-11-13 07:56:54 -0300

    adicionando mixin no arquivo

--- lib/conta.dart
+++ lib/conta.dart
@@ -20,6 +20,46 @@
       print("O Titular: $titular Saldo: R\$ $_saldo");
     }
 }
+
+mixin Imposto{
+  double taxa = 0.03;
+
+  double valorTaxado(double valor){
+    return valor * taxa;
+  }
+}
+
+class ContaEmpresa extends Conta with Imposto{
+  ContaEmpresa(super.titular,super._saldo);
+
+  @override
+  void enviar(double valor) {
+    if(_saldo >= valor + valorTaxado(valor)){
+      _saldo -= _saldo + valorTaxado(valor); 
+    }
+   
+  }
+
+  @override
+  void receber(double valor) {
+   _saldo += valor - valorTaxado(valor);
+  }
+}
+
+class ContaInvestimento extends Conta with Imposto{
+  ContaInvestimento(super.titular,super._saldo);
+
+  @override
+  void enviar(double valor) {
+   
+  }
+
+  @override
+  void receber(double valor) {
+   
+  }
+}
+
 class ContaCorrente extends Conta{
   double emprestimo = 300;
   ContaCorrente(super.titular,super._saldo);
@@ -32,6 +72,7 @@ class ContaCorrente extends Conta{
         }
     }
 }
+
 class ContaPoupanca extends Conta{
   double rendimento = 0.05;
 
